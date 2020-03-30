aa1a0772407d8a49d2e9f431c22d291e594b944c
Jane <cjmendez26@gmail.com>
2020-03-29 19:15:44 -0700

    html changes

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ document.getElementById("generate").addEventListener("click", generatePassword);
 function generator(){
     var num = '0, 1, 2, 3, 4, 5, 6, 7, 8, 9';
     var abcs = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z';
-    var specialChar = '!, @, #, $, %, ^, &, *, (, ), _, -, +, =, {, [, }, ], <, >, ?, /, ~';
+    var specialChar = '!, @, #, $, %, ^, &, *, _, -, +, =, ?, /, ~';
 
-    for(var i=0; i<length; i++) {
-        // generate a random character from (num, abcs, or specialChar) 
-        // to add to the generated password
+// generate a random character from (num, abcs, or specialChar) 
+// to add to the generated password
+    for(var i = 0; i< length; i++) { 
+        
     }
 }
 
 function generatePassword() {
+
     var length = document.getElementById("length").value;
+    console.log("generate password");
+    console.log(length);
     generator();
 }
